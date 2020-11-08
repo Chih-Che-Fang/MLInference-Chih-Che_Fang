ce13800ed337e2925eabaa882c2a50e92bda4bda
s961206 <s961206@gmail.com>
2020-11-08 15:07:37 -0500

    1st version

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MLInference-Chih-Che_Fang
+# The source code owned by me, I don't share this source with anyone  
+# Please grade groupwork individually  
---
+++ run_test.bat
@@ -0,0 +1,14 @@
+REM kill all docker process
+@ECHO OFF
+FOR /f "tokens=*" %%i IN ('docker ps -q') DO docker kill %%i
+
+REM run docker image
+docker run -d -p 5000:5000 myimage
+
+REM Wait for server to be ready
+PING localhost -n 10 >NUL
+
+REM perfrom MLInference test
+curl -X POST -F file=@dog1.jpg -F file=@dog2.jpg -F file=@cat1.jpg -F file=@cat2.jpg -F file=@squirrel1.jpg http://localhost:5000/predict
+
+pause
